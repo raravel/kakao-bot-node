4c2824079a485de362c93c134b65031da792bb59
Tree Some <youn@tree-some.dev>
2020-06-07 20:22:58 +0900

    cmdlist분리

--- index.js
+++ index.js
@@ -10,68 +10,14 @@ const BSON = require('bson');
 const Kaling = require('./modules/kaling.js');
 const kakao = require('./node-kakao');
 const M = require('./modules/common.js');
-require('./modules/polling.js');
+//require('./modules/polling.js');
 
 
 // global defins
 const client = new kakao.TalkClient('youn');
 const Long = BSON.Long;
 const config = require('./config.json');
-
-
-const commandList = {
-    "등록": (chat) => {
-		M.isAdmin(chat)
-			.then(() => {
-				const idx = global.channels.findIndex(c => c.toNumber() === chat.channel.id.toNumber());
-				if ( idx >= 0 ) {
-					chat.channel.sendText("이미 등록된 방입니다.");
-					return;
-				}
-
-				global.channels.push(chat.channel.id);
-				global.rooms.push(chat.channel);
-				try {  
-					fs.writeFileSync('channels.db', JSON.stringify(global.channels, null, '\t'), { encoding: 'utf8' });
-					chat.channel.sendText(`[${chat.channel.channelInfo.name}](${chat.channel.id.toNumber()}) 을(를) 등록했습니다.`);
-					return;
-				} catch(err) {
-					chat.channel.sendText(`방 등록에 실패했습니다. ${err.message}`);
-					return;
-				}
-			})
-			.catch(err => {
-			});
-	},
-	"템": (chat) => {
-		const attachment = Kaling({
-			type: kakao.CustomType.FEED,
-			title: `환영합니다!`,
-			desc: `공지는 꼭 확인하여 주시기 바랍니다.`,
-			link: 'https://sopia-bot.github.io',
-			buttonStyle: kakao.CustomButtonStyle.VERTICAL,
-			buttons: [
-				{
-					title: '홈페이지 방문하기',
-					dpType: kakao.CustomButtonDisplayType.ALL,
-					link: 'https://sopia-bot.github.io/',
-				},
-				{
-					title: '릴리즈 노트 확인하기',
-					dpType: kakao.CustomButtonDisplayType.ALL,
-					link: 'https://sopia-bot.github.io/release/',
-				},
-			],
-			thumbnails: [
-				{
-					url: 'https://sopia-bot.github.io/%EB%A8%B8%EA%B7%B8%EC%BB%B5.jpg',
-					style: kakao.CustomImageCropStyle.ORIGINAL,
-				},
-			],
-		});
-		chat.channel.sendTemplate(attachment);
-	},
-};
+const commandList = require('./modules/cmd.js');
 
 
 (async () => {
---
+++ modules/cmd.js
@@ -0,0 +1,59 @@
+const kakao = require('../node-kakao');
+const M = require('./common.js');
+const Kaling = require('./kaling.js');
+const BSON = require('bson');
+const Long = BSON.Long;
+
+module.exports = {
+    "등록": (chat) => {
+		M.isAdmin(chat)
+			.then(() => {
+				const idx = global.channels.findIndex(c => new Long(c.low, c.high).toNumber() === chat.channel.id.toNumber());
+				if ( idx >= 0 ) {
+					chat.channel.sendText("이미 등록된 방입니다.");
+					return;
+				}
+
+				global.channels.push(chat.channel.id);
+				global.rooms.push(chat.channel);
+				try {  
+					fs.writeFileSync('channels.db', JSON.stringify(global.channels, null, '\t'), { encoding: 'utf8' });
+					chat.channel.sendText(`[${chat.channel.channelInfo.name}](${chat.channel.id.toNumber()}) 을(를) 등록했습니다.`);
+					return;
+				} catch(err) {
+					chat.channel.sendText(`방 등록에 실패했습니다. ${err.message}`);
+					return;
+				}
+			})
+			.catch(err => {
+			});
+	},
+	"템": (chat) => {
+		const attachment = Kaling({
+			type: kakao.CustomType.FEED,
+			title: `환영합니다!`,
+			desc: `공지는 꼭 확인하여 주시기 바랍니다.`,
+			link: 'https://sopia-bot.github.io',
+			buttonStyle: kakao.CustomButtonStyle.VERTICAL,
+			buttons: [
+				{
+					title: '홈페이지 방문하기',
+					dpType: kakao.CustomButtonDisplayType.ALL,
+					link: 'https://sopia-bot.github.io/',
+				},
+				{
+					title: '릴리즈 노트 확인하기',
+					dpType: kakao.CustomButtonDisplayType.ALL,
+					link: 'https://sopia-bot.github.io/release/',
+				},
+			],
+			thumbnails: [
+				{
+					url: 'https://sopia-bot.github.io/%EB%A8%B8%EA%B7%B8%EC%BB%B5.jpg',
+					style: kakao.CustomImageCropStyle.ORIGINAL,
+				},
+			],
+		});
+		chat.channel.sendTemplate(attachment);
+	},
+};
