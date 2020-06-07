f6fb1e39b368c547d4861c36f040b6ab84033955
Tree Some <youn@tree-some.dev>
2020-06-07 19:33:24 +0900

    common.js 생성

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const BSON = require('bson');
 // custom modules
 const Kaling = require('./modules/kaling.js');
 const kakao = require('./node-kakao');
+const M = require('./modules/common.js');
 
 
 // global defins
@@ -19,7 +20,7 @@ const config = require('./config.json');
 
 const commandList = {
     "등록": (chat) => {
-		isAdmin(chat)
+		M.isAdmin(chat)
 			.then(() => {
 				const idx = global.channels.findIndex(c => c.toNumber() === chat.channel.id.toNumber());
 				if ( idx >= 0 ) {
@@ -69,74 +70,6 @@ const commandList = {
 		});
 		chat.channel.sendTemplate(attachment);
 	},
-};
-
-const isAdmin = (chat) => {
-	// unguis1@naver.com
-	//const adminId = { low: -1000804380, high: 28493638, unsigned: false };
-	const adminId = 122379246648225760;
-	const senderId = chat.sender.id;
-
-	return new Promise((resolve, reject) => {
-
-	if ( senderId.toNumber() === adminId ) {
-		resolve();
-		return;
-	}
-
-	let roomAdminId = {};
-	chat.channel.client.openChatManager.getLinkOwner(chat.channel.linkId)
-		.then(chatUser => {
-			roomAdminId = chatUser.id;
-			if (roomAdminId.toNumber() === senderId.toNumber() ) {
-				resolve();
-			} else {
-				reject(new Error('not match admin id'));
-			}
-		})
-		.catch(reject);
-	});
-}
-
-const isCmd = (e) => {
-    const sig = "!";
-    let msg = e.text;
-    if ( msg.indexOf(sig) === 0 ) {
-        msg = msg.replace(sig, "");
-        e.message = msg;
-        e.cmd = msg.split(' ')[0];
-        e.isCmd = true;
-        e.content = msg.replace(e.cmd, "").trim();
-        return true;
-    }
-    return false;
-};
-
-
-const runCmd = (cmd, e) => {
-    let str = "";
-    switch ( typeof cmd ) {
-        case "string": {
-            str = cmd;
-        } break;
-        case "function": {
-            str = cmd(e);
-        } break;
-        case "object": {
-            if ( Array.isArray(cmd) ) {
-                let result = Math.floor(Math.random() * cmd.length);
-                switch ( typeof cmd[result] ) {
-                    case "string": {
-                        str = cmd[result];
-                    } break;
-                    case "function": {
-                        str = cmd[result](e);
-                    }
-                }
-            }
-        } break;
-    }
-    return str;
 };
 
 const sendToAllChannels = (msg) => {
@@ -259,10 +192,10 @@ const createRecommandLives = async () => {
 
 	client.on('message', async (chat) => {
 
-		if ( isCmd(chat) ) {
+		if ( M.isCmd(chat) ) {
 			cmd = commandList[chat.cmd];
 			if ( cmd ) {
-				result = runCmd(cmd, chat);
+				result = M.runCmd(cmd, chat);
 				if ( result ) {
 					if ( typeof result === "string" ) {
 						chat.channel.sendText(result);
---
+++ modules/common.js
@@ -0,0 +1,67 @@
+exports.isAdmin = (chat) => {
+	// unguis1@naver.com
+	//const adminId = { low: -1000804380, high: 28493638, unsigned: false };
+	const adminId = 122379246648225760;
+	const senderId = chat.sender.id;
+
+	return new Promise((resolve, reject) => {
+
+	if ( senderId.toNumber() === adminId ) {
+		resolve();
+		return;
+	}
+
+	let roomAdminId = {};
+	chat.channel.client.openChatManager.getLinkOwner(chat.channel.linkId)
+		.then(chatUser => {
+			roomAdminId = chatUser.id;
+			if (roomAdminId.toNumber() === senderId.toNumber() ) {
+				resolve();
+			} else {
+				reject(new Error('not match admin id'));
+			}
+		})
+		.catch(reject);
+	});
+}
+
+exports.isCmd = (e) => {
+    const sig = "!";
+    let msg = e.text;
+    if ( msg.indexOf(sig) === 0 ) {
+        msg = msg.replace(sig, "");
+        e.message = msg;
+        e.cmd = msg.split(' ')[0];
+        e.isCmd = true;
+        e.content = msg.replace(e.cmd, "").trim();
+        return true;
+    }
+    return false;
+};
+
+
+exports.runCmd = (cmd, e) => {
+    let str = "";
+    switch ( typeof cmd ) {
+        case "string": {
+            str = cmd;
+        } break;
+        case "function": {
+            str = cmd(e);
+        } break;
+        case "object": {
+            if ( Array.isArray(cmd) ) {
+                let result = Math.floor(Math.random() * cmd.length);
+                switch ( typeof cmd[result] ) {
+                    case "string": {
+                        str = cmd[result];
+                    } break;
+                    case "function": {
+                        str = cmd[result](e);
+                    }
+                }
+            }
+        } break;
+    }
+    return str;
+};
